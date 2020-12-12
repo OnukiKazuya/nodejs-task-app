584b132ee65e4935c47bb5f27a3e2900e0ddae61
Kazuya <onuki.kazuya@outlook.com>
2020-12-12 11:47:57 +0900

    =test

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ app.use(Task_Router);
 app.listen(port, () => {
   console.log("Server is up on port : " + port);
   console.log("process.env.PORT : ", process.env.PORT)
-  console.log("process.env.JWT_TOKEN : ", process.env.JWT_TOKEN)
+  console.log("process.env.JWT_SECRET : ", process.env.JWT_SECRET)
   console.log("process.env.MONGODB_URL : ", process.env.MONGODB_URL)
 });
 
